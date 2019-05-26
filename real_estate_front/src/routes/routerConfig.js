3aef0be7f5131891604acf5485810263dbe680dc
Andrey <a.v.vlasov86@gmail.com>
2019-05-26 13:18:47 +0300

    base configuration

--- real_estate_front/src/routes/routerConfig.js
+++ real_estate_front/src/routes/routerConfig.js
@@ -5,27 +5,28 @@ import Announcements from '../components/Pages/Announcements/Announcements';
 const routerConfig = [
 
 	{
-		id: 2,
+		id: 1,
 		path: "/announcements",
 		component: Announcements,
         icon: 'fa fa-fw fa-home',
-        name: 'Test',
+        name: 'Announcements',
 		show: true,
         subitems: [
 
 		]
 	},
 	{
-		id: 1,
-		path: "*",
+		id: 0,
+		path: "/",
 		component: DefaultPage,
         icon: '',
-        name: 'Test',
+        name: 'Default',
 		show: false,
         subitems: [
 
 		]
 	},
+
 
 ];
 
